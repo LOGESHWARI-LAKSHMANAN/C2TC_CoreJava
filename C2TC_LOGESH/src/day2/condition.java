259c514d09a4753396e9203d5b2e33970fadbbeb
LOGESHWARI-LAKSHMANAN <logeshwari0808@gmail.com>
2024-09-04 13:22:08 +0530

    THIS IS FIRST PROJECT

--- C2TC_LOGESH/src/day2/condition.java
+++ C2TC_LOGESH/src/day2/condition.java
@@ -1,5 +1,26 @@
 package day2;
 
 public class condition {
+	public static void main(String args[]) {
+		//if condition
+		int i=10;
+		 if(i>8)
+			 System.out.println("i is greater than 8");
+		 //if else
+		 int a= 10,b= 20;
+		 if(a>b)
+           System.out.println("a is greater than b");
+		 else
+			 System.out.println("b is greater than a");
+		 //nested if
+		 int e=10,f=1;
+		 if(e>f)
+			 System.out.println("e is greater than f");
+		    if (e==10)
+		    	System.out.println("e is ten");
+		    else
+		    	System.out.println("e is not ten");	
+
+	}
 
 }
