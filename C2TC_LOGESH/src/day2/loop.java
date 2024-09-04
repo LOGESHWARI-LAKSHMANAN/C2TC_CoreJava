d05e3480b3404663001f136220647f053ee7f256
LOGESHWARI-LAKSHMANAN <logeshwari0808@gmail.com>
2024-09-04 14:16:44 +0530

    THIS IS FIRST PROJECT

---
+++ C2TC_LOGESH/src/day2/loop.java
@@ -0,0 +1,24 @@
+package day2;
+
+public class loop {
+	public static void main(String args[]) {
+		//for loop
+		for (int i = 0; i <10; i++) {
+			  System.out.println(i);
+			}
+		//while loop
+		int i = 10;
+		while (i < 20) {
+		  System.out.println(i);
+		  i++;
+		}
+		//do-while loop
+		int j=0;
+		do {
+		  System.out.println(i);
+		  j++;
+		}
+		while (j<3);
+
+}
+}
